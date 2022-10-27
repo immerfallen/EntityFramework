b082e608e2dc8d5f853d5921bd64a293360e5469
immerfallen <marodemelo10@gmail.com>
2022-10-27 20:24:37 -0300

    Add project files.

---
+++ EntityFramework.ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFramework.ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
